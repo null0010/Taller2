f740a87019b9a29c54176f762a69ca70aae63106
NullCoder <90267124+null0010@users.noreply.github.com>
2021-12-02 10:44:34 +0900

    Update Asignatura.java

--- src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/Asignatura.java
+++ src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/Asignatura.java
@@ -1,17 +1,21 @@
 package cl.ucn.ei.pa.sistemaUniversidadUCR.dominio;
 
+import cl.ucn.ei.pa.sistemaUniversidadUCR.logica.ListaParalelos;
+
 public class Asignatura {
     private int codigo;
     private String nombre;
     private int creditos;
     private double notaFinal;
     //private ListaUsuarios listaEstudiantes;
+    private ListaParalelos listaParalelos;
 
     public Asignatura(int codigo, String nombre, int creditos) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.creditos = creditos;
         this.notaFinal = 0.0;
+        this.listaParalelos = new ListaParalelos(1000);
     }
 
     public Asignatura(int codigo) {
@@ -48,5 +52,13 @@ public class Asignatura {
 
     public void setNotaFinal(double notaFinal) {
         this.notaFinal = notaFinal;
+    }
+
+    public ListaParalelos getListaParalelos() {
+        return listaParalelos;
+    }
+
+    public void setListaParalelos(ListaParalelos listaParalelos) {
+        this.listaParalelos = listaParalelos;
     }
 }

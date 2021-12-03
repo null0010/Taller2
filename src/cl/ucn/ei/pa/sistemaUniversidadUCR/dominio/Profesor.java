0ac4766b6916aac2396a2fddb25c2393e4e60d66
NullCoder <90267124+null0010@users.noreply.github.com>
2021-12-04 07:07:57 +0900

    Update Profesor.java

--- src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/Profesor.java
+++ src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/Profesor.java
@@ -1,13 +1,16 @@
 package cl.ucn.ei.pa.sistemaUniversidadUCR.dominio;
 
+import cl.ucn.ei.pa.sistemaUniversidadUCR.logica.ListaParalelos;
+
 public class Profesor extends Usuario {
     private int salario;
-    //private ListaParalelos listaParalelos
+    private ListaParalelos listaParalelos;
     //private ListaAsignaturas listaAsignaturasDictando
 
     public Profesor(String rut, String correo, String contrasena, int salario) {
         super(rut, correo, contrasena);
         this.salario = salario;
+        this.listaParalelos = new ListaParalelos(1000);
     }
 
     public int getSalario() {
@@ -16,5 +19,13 @@ public class Profesor extends Usuario {
 
     public void setSalario(int salario) {
         this.salario = salario;
+    }
+
+    public ListaParalelos getListaParalelos() {
+        return listaParalelos;
+    }
+
+    public void setListaParalelos(ListaParalelos listaParalelos) {
+        this.listaParalelos = listaParalelos;
     }
 }

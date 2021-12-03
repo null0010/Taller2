eff186c04d7e434a6fe0620cc9ab629a38f6234e
NullCoder <90267124+null0010@users.noreply.github.com>
2021-12-04 07:07:55 +0900

    Update Paralelo.java

--- src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/Paralelo.java
+++ src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/Paralelo.java
@@ -1,17 +1,14 @@
 package cl.ucn.ei.pa.sistemaUniversidadUCR.dominio;
 
-import cl.ucn.ei.pa.sistemaUniversidadUCR.logica.ListaAsignaturas;
 import cl.ucn.ei.pa.sistemaUniversidadUCR.logica.ListaUsuarios;
 
 public class Paralelo {
     private int numero;
-    private ListaAsignaturas listaAsignaturas;
     private ListaUsuarios listaEstudiantes;
     private Profesor profesor;
 
     public Paralelo(int numero) {
         this.numero = numero;
-        this.listaAsignaturas = new ListaAsignaturas(1000);
         this.listaEstudiantes = new ListaUsuarios(1000);
     }
 
@@ -29,14 +26,6 @@ public class Paralelo {
 
     public void setProfesor(Profesor profesor) {
         this.profesor = profesor;
-    }
-
-    public ListaAsignaturas getListaAsignaturas() {
-        return listaAsignaturas;
-    }
-
-    public void setListaAsignaturas(ListaAsignaturas listaAsignaturas) {
-        this.listaAsignaturas = listaAsignaturas;
     }
 
     public ListaUsuarios getListaEstudiantes() {

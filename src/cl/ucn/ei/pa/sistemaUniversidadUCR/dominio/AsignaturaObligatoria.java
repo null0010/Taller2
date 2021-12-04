cec62265490da7f88063db96cc50325733ca8dc1
NullCoder <90267124+null0010@users.noreply.github.com>
2021-12-04 09:44:54 +0900

    Update AsignaturaObligatoria.java

--- src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/AsignaturaObligatoria.java
+++ src/cl/ucn/ei/pa/sistemaUniversidadUCR/dominio/AsignaturaObligatoria.java
@@ -1,15 +1,18 @@
 package cl.ucn.ei.pa.sistemaUniversidadUCR.dominio;
 
 import cl.ucn.ei.pa.sistemaUniversidadUCR.logica.ListaAsignaturas;
+import cl.ucn.ei.pa.sistemaUniversidadUCR.logica.ListaParalelos;
 
 public class AsignaturaObligatoria extends Asignatura {
     private int nivel;
     private ListaAsignaturas listaAsignaturasPrerrequisitos;
+    private ListaParalelos listaParalelos;
 
     public AsignaturaObligatoria(int codigo, String nombre, int creditos, int nivel) {
         super(codigo, nombre, creditos);
         this.nivel = nivel;
         this.listaAsignaturasPrerrequisitos = new ListaAsignaturas(1000);
+        this.listaParalelos = new ListaParalelos(4);
     }
 
     public int getNivel() {
@@ -26,5 +29,13 @@ public class AsignaturaObligatoria extends Asignatura {
 
     public void setListaAsignaturasPrerrequisitos(ListaAsignaturas listaAsignaturasPrerrequisitos) {
         this.listaAsignaturasPrerrequisitos = listaAsignaturasPrerrequisitos;
+    }
+
+    public ListaParalelos getListaParalelos() {
+        return listaParalelos;
+    }
+
+    public void setListaParalelos(ListaParalelos listaParalelos) {
+        this.listaParalelos = listaParalelos;
     }
 }

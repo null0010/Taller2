a88fe21bcb2da1f9e91796d6e5fb98c89e732d33
NullCoder <90267124+null0010@users.noreply.github.com>
2021-12-04 09:45:02 +0900

    Update SistemaUniversidadUCR.java

--- src/cl/ucn/ei/pa/sistemaUniversidadUCR/logica/SistemaUniversidadUCR.java
+++ src/cl/ucn/ei/pa/sistemaUniversidadUCR/logica/SistemaUniversidadUCR.java
@@ -73,37 +73,144 @@ public interface SistemaUniversidadUCR {
      */
     boolean ingresarAsignaturaPrerrequisito(int codigo);
 
+
+    /**
+     * Se ingresa un profesor al sistema.
+     * @param rut corresponde al rut del profesor.
+     * @param correo Corresponde al correo del profesor.
+     * @param contrasena Corresponde a la contraseña del profesor.
+     * @param salario Corresponde al sueldo del profesor.
+     * @return Retorna un boolean que da a conocer si se ingresó con exito o no.
+     */
     boolean ingresarProfesor(String rut, String correo, String contrasena, int salario);
-
+    /**
+     * Ingresa un paralelo a la lista general de paralelos.
+     * @param numeroParalelo Numero del paralelo.
+     * @param codigoAsignatura Codigo de la asignatura a la que pertenece el paralelo.
+     * @param rutProfesor Rut del profesor al que pertenece el paralelo.
+     * @return Retorna un boleano que indica si se ingresó con exito el paralelo.
+     */
     boolean ingresarParalelo(int numeroParalelo, int codigoAsignatura, String rutProfesor);
-
+    /**
+     * Revisa si un usuario se encuentra registrado.
+     * @param correo Correo que se verificará.
+     * @return Retorna un booleano que indica si el usuario está registrado o no.
+     */
     boolean isUsuarioRegistrado(String correo);
-
+    /**
+     * Revisa si el usuario ingresado corresponde a un profesor.
+     * @param correo Correo que corresponde al usuario.
+     * @return Retorna un booleano uqe indica si es profesor o no.
+     */
     boolean isUsuarioProfesor(String correo);
-
+    /**
+     * Verifica si el usuario ingresado es administrador.
+     * @param correo Correo del ususario ingresado.
+     * @param contrasena Contraseña del usuario ingresado.
+     * @return Retorna un booleano que indica si el usuario es administrador o no-
+     */
     boolean isUsuarioAdministrador(String correo, String contrasena);
-
-    boolean isContraseñaCorrecta(String correo, String contrasena);
-
+    /**
+     * Verifica que la contraseña ingresada es correcta,
+     * @param correo Correo de el usuario a verificar.
+     * @param contrasena Contraseña del usuario a verificar.
+     * @return Retorna un booleano que indica si la contraseña del usuario es correcta.
+     */
+    boolean isContrasenaCorrecta(String correo, String contrasena);
+    /**
+     * Verifica si la fecha ingresada corresponde a inicio de semestre.
+     * @param fecha Fecha ingresada qu se verificará.
+     * @return Retorna un booleano que indicará si la fecha corresponde al inicio de semestre.
+     */
     boolean isInicioSemestre(String fecha);
-
+    /**
+     * Verifica si la fecha ingresada corresponde a mitad de semestre.
+     * @param fecha Fecha ingresada qu se verificará.
+     * @return Retorna un booleano que indicará si la fecha corresponde a mitad de semestre.
+     */
     boolean isMitadSemestre(String fecha);
-
+    /**
+     * Verifica si la fecha ingresada corresponde a final de semestre.
+     * @param fecha Fecha ingresada qu se verificará.
+     * @return Retorna un booleano que indicará si la fecha corresponde al final de semestre.
+     */
     boolean isFinalSemestre(String fecha);
-
+    /**
+     * Verifica si la fecha ingresada corresponde a cierre de semestre.
+     * @param fecha Fecha ingresada qu se verificará.
+     * @return Retorna un booleano que indicará si la fecha corresponde al cierre de semestre.
+     */
     boolean isCierreSemestre(String fecha);
-
+    /**
+     * Verifica si la fecha ingresada corresponde a las vacaciones.
+     * @param fecha Fecha ingresada qu se verificará.
+     * @return Retorna un booleano que indicará si la fecha corresponde a vacacione.
+     */
     boolean isVacaciones(String fecha);
-
+    /**
+     * Se obtienen los datos de las asignaturas que podría inscribir el estudiante.
+     * @param correoEstudiante correo del estudiante que inscribirá las asignaturas.
+     * @return retorna las asignaturas que el estudiante puede inscribir.
+     */
     String obtenerDatosAsignaturasInscribiblesEstudiante(String correoEstudiante);
-
+    /**
+     * Se obtienen los datos para una asignatura en especifico.
+     * @param codigoAsignatura Codigo de la asignatura a de lacual se quieren saber los paralelos.
+     * @return  Retorna la informacion de la asignatura consultada.
+     */
     String obtenerDatosParalelosAsignatura(int codigoAsignatura);
-
+    /**
+     * Se obtienen los datos de las asignaturas que el estudiante ya ha inscrito.
+     * @param correoEstudiante Correo del estudiante al que se le consultarán los datos.
+     * @return  Retorna las asignaturas que ha ionscrito el estudiante.
+     */
     String obtenerDatosAsignaturasInscritasEstudiante(String correoEstudiante);
-
+    /**
+     * Se elimina una asignatura inscrita por un estudiante.
+     * @param correoEstudiante Correo del estudiante al que se el eliminará la asignatura.
+     * @param codigoAsignatura Codigo de la asignatura que desea elminar el estudiante.
+     * @return Retorna si se elminó al estudiante de la asignatura.
+     */
     boolean eliminarAsignaturaInscritaEstudiante(String correoEstudiante, int codigoAsignatura);
-
+    /**
+     * Se obtienen los paralelos que está dictando el profesor.
+     * @param correoProfesor Correo del profesor.
+     * @return Retorna los paralelos del profesor.
+     */
     String obtenerParalelosProfesor(String correoProfesor);
-
+    /**
+     * Se obtienen los estudiantes de un paralelo seleccionado por el profesor
+     * @param correoProfesor Correo del profesor.
+     * @param numeroParalelo Numero del paralelo que digitó el profesor.
+     * @return  Retorna los alumnos del paralelo seleccionado por el profesor.
+     */
     String obtenerEstudiantesParaleloProfesor(String correoProfesor, int numeroParalelo);
+
+    /**
+     * Se obtiene las asignaturas que dicta el profesor
+     * @return String
+     */
+    String obtenerAsignaturasProfesor(String correoProfesor);
+
+
+    /**
+     * Se obtienen los datos de cada uno de los estudiante del profesor
+     * @param correoProfesor
+     * @return
+     */
+    String obtenerEstudiantesAsignatura(String correoProfesor, int codigoAsignatura);
+
+    /**
+     * Se ingresa la nota final a la asignatura de un estudiante.
+     * @param correoEstudiante Correo del estudiante
+     * @param codigoAsignatura Codigo del estudiante
+     * @param notaFinal Nota final de la asignatura de un estudiante
+     */
+    void ingresarNotaFinalAlumno(String correoEstudiante, int codigoAsignatura, double notaFinal);
+
+    /**
+     * Se obtienen los datos de cada uno de los estudiantes.
+     * @return String
+     */
+    String obtenerDatosEstudiantes();
 }
